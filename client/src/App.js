c033a0d0e33a85e2587f0ec8da5d520a6dca9670
shutsugan <muhammad@nextmedia.ma>
2018-10-31 20:26:26 +0100

    refactor (App):
    remove the Component from the import
    transform the component to a stateless
    functional component

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,29 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {ApolloProvider} from 'react-apollo';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import {Provider} from 'react-redux';
-import HomeView from './views/HomeView';
+
 import client from './ApolloClient';
 import store from './store';
+
+import HomeView from './views/HomeView';
 import ApartmentView from "./views/ApartmentView";
 
-class App extends Component {
-  render() {
-    return (
-      <ApolloProvider client={client}>
-        <Provider store={store}>
-          <Router>
-            <div>
-              <Route exact path="/" component={HomeView}/>
-              <Route exact path="/apartments/:apartmentId" component={ApartmentView}/>
-            </div>
-          </Router>
-        </Provider>
-      </ApolloProvider>
-    );
-  }
-}
+const App = _ => (
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <Router>
+        <div>
+          <Route exact path="/" component={HomeView}/>
+          <Route
+            exact
+            path="/apartments/:apartmentId"
+            component={ApartmentView}
+          />
+        </div>
+      </Router>
+    </Provider>
+  </ApolloProvider>
+);
 
 export default App;
